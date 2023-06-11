ca578c020e1360cfe4f426367d3c98c4b23d5608
siegfriedbz <siegfried.bozza@yahoo.com>
2023-06-11 17:18:33 +0200

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,17 @@ Rails.application.routes.draw do
   devise_for :users,
   path: '',
   path_names: { 
-    sign_in: 'login',
-    sign_out: 'logout',
-    registration: 'signup'
+    sign_in: 'api/v1/login',
+    sign_out: 'api/v1/logout',
+    registration: 'api/v1/signup'
   },
   controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations' 
+    sessions: 'api/v1/users/sessions',
+    registrations: 'api/v1/users/registrations' 
   }
 
+  get 'home/check'
   root to: 'components#index'
   # get 'components/index'
-  get '/*path', to: 'components#index', via: :all, constraints: lambda { |req| req.path.exclude?('api/v1') && !req.path.include?('signup') }
-
-
-  get 'home/check'
+  get '/*path', to: 'components#index', via: :all, constraints: lambda { |req| req.path.exclude?('api/v1') }
 end
