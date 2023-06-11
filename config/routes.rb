7a6f905c5b57ef967728d3e6ee19f92236305db5
siegfriedbz <siegfried.bozza@yahoo.com>
2023-06-11 15:41:05 +0200

    update routes for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
-  root 'components#index'
-  # get 'components/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users,
+  path: '',
+  path_names: { 
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations' 
+  }
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'components#index'
+  # get 'components/index'
+  get '/*path', to: 'components#index', via: :all, constraints: lambda { |req| req.path.exclude?('api/v1') && !req.path.include?('signup') }
+
+
   get 'home/check'
 end
