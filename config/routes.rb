5ab5345de977bde05611d133a76a8e3fb7e0bef3
siegfriedbz <siegfried.bozza@yahoo.com>
2023-06-11 11:33:08 +0200

    setup react

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'components#index'
+  # get 'components/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
