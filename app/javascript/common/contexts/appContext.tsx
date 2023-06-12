b162af863df91dae21bce3411da365bcb5283ff2
siegfriedbz <siegfried.bozza@yahoo.com>
2023-06-12 12:29:57 +0200

    add appContext

---
+++ app/javascript/common/contexts/appContext.tsx
@@ -0,0 +1,22 @@
+import React, { useState, createContext, useContext } from 'react'
+import { IFlashMessage, IAppContext } from '../utils/interfaces'
+
+export const initFlashMessage = {
+    message: null,
+    type: "success",
+}
+
+const AppContext = createContext(null)
+
+export const useAppContext = (): IAppContext => useContext(AppContext)
+
+export const AppContextProvider: React.FC = ({ children }: any ) => {
+    const [flashMessage, setFlashMessage] = useState<IFlashMessage>(initFlashMessage)
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+
+    return (
+        <AppContext.Provider value={{ flashMessage, setFlashMessage, isLoading, setIsLoading }}>
+            { children }
+        </AppContext.Provider>
+    )
+}
