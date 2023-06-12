42160338e8e406c76d2c40cab458e4f79d78eda2
siegfriedbz <siegfried.bozza@yahoo.com>
2023-06-12 12:32:11 +0200

    add AppContextProvider + UserContextProvider

--- app/javascript/common/index.tsx
+++ app/javascript/common/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { AppContextProvider, UserContextProvider } from './contexts'
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = ReactDOM.createRoot(document.getElementById('root'))
     root.render(
-        <App />
+        <AppContextProvider>
+            <UserContextProvider>
+                <App />
+            </UserContextProvider>
+        </AppContextProvider>
     )
-});
+})
