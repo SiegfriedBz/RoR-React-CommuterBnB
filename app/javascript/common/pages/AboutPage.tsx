8e9fe894749339f64e07c69b9ffcda79d273ab75
siegfriedbz <siegfried.bozza@yahoo.com>
2023-06-12 12:34:08 +0200

    add AboutPage + MessagesPage

---
+++ app/javascript/common/pages/AboutPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AboutPage: React.FC = () => {
+    return (
+        <div>
+            <h1>AboutPage</h1>
+            <p>
+            I'm baby gorpcore synth typewriter semiotics, crucifix cliche polaroid kitsch williamsburg scenester chillwave YOLO jianbing leggings vinyl. DIY kale chips helvetica lo-fi polaroid shaman. Big mood sustainable flexitarian, iPhone deep v ascot bicycle rights vexillologist air plant organic jianbing trust fund. Knausgaard enamel pin literally ascot. Tilde meggings vexillologist, art party gastropub ennui marxism mukbang ascot affogato paleo pinterest af hella try-hard. Vice photo booth lyft, organic selvage austin man braid deep v.
+            </p>
+        </div>
+    )
+};
+
+export default AboutPage
