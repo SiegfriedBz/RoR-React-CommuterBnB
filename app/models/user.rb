f9fcbdfa0aeed160a04d3a19f41b0086a8e25639
siegfriedbz <siegfried.bozza@yahoo.com>
2023-06-12 13:03:57 +0200

    fix user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
 
   def jwt_payload
     super.merge(user_id: self.id, email: self.email, role: self.role)
-    super.merge(user_id: self.id, email: self.email)
   end
 
   enum role: { user: 0, admin: 1 }
