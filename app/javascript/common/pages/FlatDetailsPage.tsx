785b928a9b571573821ec3f2e9fd1b3232259cc4
siegfriedbz <siegfried.bozza@yahoo.com>
2023-06-13 19:51:26 +0200

    add FlatCardCarousel + MapView to FlatDetailsPage

--- app/javascript/common/pages/FlatDetailsPage.tsx
+++ app/javascript/common/pages/FlatDetailsPage.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import { useFlatsContext } from '../contexts'
+import FlatCardCarousel from '../components/flats/FlatCardCarousel'
+import MapView from '../components/map/MapView'
 
 const FlatDetailsPage: React.FC = () => {
     const { id } = useParams()
@@ -12,10 +14,7 @@ const FlatDetailsPage: React.FC = () => {
     const flat = flats.find(flat => flat.flatId === parseInt(id))
     if(!flat) return null
 
-    console.log("FlatDetailsPage flat", flat)
-
-    const { ownerId, title, description, address, longitude, latitude } = flat
-
+    const { flatId, ownerId, title, description, address, longitude, latitude } = flat
     // 1. render flat with infos fetched from context (from previous getAllFlats fetch)
     // 2. fetch flat details from backend (getFlatWithOwnerDetailsAndFlatReviews) + update context for this flat
     // 2': render loading spinners for flat details not yet fetched
@@ -27,8 +26,10 @@ const FlatDetailsPage: React.FC = () => {
             <h2>{title}</h2>
             <p>{description}</p>
             <p>{address}</p>
-            <p>{longitude}</p>
-            <p>{latitude}</p>
+            <br />
+            <FlatCardCarousel flat={flat} />
+            <br />
+            <MapView selectedFlatId={flatId}/>    
         </div>
     )
 }
