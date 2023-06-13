c36bcee633717efd3115ecde9bcf5ce9cf7ae0c7
siegfriedbz <siegfried.bozza@yahoo.com>
2023-06-13 16:39:51 +0200

    update imports

--- app/javascript/common/components/index.ts
+++ app/javascript/common/components/index.ts
@@ -1,5 +1,9 @@
 import FlashMessage from './FlashMessage'
+import FlatForm from './flats/FlatForm'
+import FlatsContainer from './flats/FlatsContainer'
 
 export {
-    FlashMessage
+    FlashMessage,
+    FlatForm,
+    FlatsContainer
 }
