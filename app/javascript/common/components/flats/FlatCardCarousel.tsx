deee3d56bdcd1bc9aa2b99be0f85ff9b0598ce43
siegfriedbz <siegfried.bozza@yahoo.com>
2023-06-13 16:32:26 +0200

    add FlatCardCarousel

---
+++ app/javascript/common/components/flats/FlatCardCarousel.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+
+const FlatCardCarousel: React.FC<string[]> = ({ images }) => {
+    
+    if(!images) return null
+
+    const carouselIndicators = () => {
+        return (
+            <>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+            {images
+                .filter((image: string, index: number) => index > 0)
+                .map((image: string, index: number) => {
+                    return (
+                        <button key={index} type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to={index} aria-label={`Slide ${index + 1}`}></button>
+                    )
+                })
+            }
+            </>
+        )
+    }
+
+    const carouselItems = () => {
+        return (
+            <>
+                <div className="carousel-item active">
+                    <img src={images[0]} className="d-block w-100" alt="..."/>
+                </div>
+                {images
+                    .filter((image: string, index: number) => index > 0)
+                    .map((image: string, index: number) => {
+                        return (
+                            <div key={index} className="carousel-item">
+                                <img src={image} className="d-block w-100" alt="..."/>
+                            </div>
+                        )
+                    })
+                }
+            </>
+        )
+    }
+
+    return (
+        <div id="carouselExampleIndicators" className="carousel slide">
+            <div className="carousel-indicators">
+                {carouselIndicators()}
+            </div>
+            <div className="carousel-inner">
+                {carouselItems()}
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+            </button>
+        </div>
+    )
+}
+
+export default FlatCardCarousel
