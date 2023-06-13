7e377c460009447bc73e267e4a55f54c35e33bcc
siegfriedbz <siegfried.bozza@yahoo.com>
2023-06-13 16:37:54 +0200

    update UserContext to set user from token

--- app/javascript/common/contexts/userContext.tsx
+++ app/javascript/common/contexts/userContext.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, createContext, useContext } from 'react'
 import { useLocalStorage } from '../hooks'
 import { IUser, IUserContext } from '../utils/interfaces'
+import jwt_decode from 'jwt-decode';
 
 const initUser = {
     userId: undefined,
@@ -15,20 +16,25 @@ export const useUserContext = () => useContext<IUserContext>(UserContext)
 export const UserContextProvider: React.FC = ({ children }) => {
     const [user, setUser] = useState<IUser>(initUser)
 
-    // onSignUp or onLogin, set token in localStorage => set token in user state
-    // onLogout, set token in localStorage to null => set token in user state to null
     const [tokenInStorage, setTokenInStorage] = useLocalStorage('bnbToken', null)
 
     useEffect(() => {
-        if (typeof tokenInStorage === 'string') {
-            const deserializedToken = JSON.parse(tokenInStorage)
-
-            setUser({
-                userId: deserializedToken?.user_id,
-                email: deserializedToken?.email,
-                role: deserializedToken?.role
-            })
+        if (typeof tokenInStorage !== 'string' || tokenInStorage === '{}') {
+            setUser(initUser)
+            return
         }
+        const deserializedToken = JSON.parse(tokenInStorage)
+        const decodedToken = jwt_decode(deserializedToken)
+
+        if(!decodedToken) return 
+
+        const { user_id, email, role } = decodedToken
+
+        setUser({
+            userId: user_id || undefined,
+            email: email || undefined,
+            role: role || undefined
+        })
     }, [tokenInStorage])
 
 return (
