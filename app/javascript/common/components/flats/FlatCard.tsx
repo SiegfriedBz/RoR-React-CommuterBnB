ecc09c5fd4239217c60a4c374d34ae77bfd677ea
siegfriedbz <siegfried.bozza@yahoo.com>
2023-06-14 10:01:29 +0200

    add footer to FlatCard

--- app/javascript/common/components/flats/FlatCard.tsx
+++ app/javascript/common/components/flats/FlatCard.tsx
@@ -6,15 +6,17 @@ import FlatCardCarousel from './FlatCardCarousel'
 const FlatCard: React.FC<IFlat> = ({ flat }) => {
         
     return (
-        <div className="card">
-            {flat?.images && <FlatCardCarousel flat={flat} className="card-img-top" />}
+        <div className="card h-100">
+            {flat?.images && <FlatCardCarousel key={flat.flatId} flat={flat} className="card-img-top" />}
             <div className="card-body">
                 <h6 className="card-title">{flat.title}</h6>
                 <div className="d-flex justify-content-between align-content-center">
                     <span className="card-text justify-content-start">{flat.address}</span>
                     <span className="card-text">$ {flat.pricePerNightInCents/100}</span>
                 </div>
-                <Link to={`/properties/${flat.flatId}`} className="btn btn-outline-primary w-100 mt-2">Visit Flat</Link>
+            </div>
+            <div className="card-footer bg-transparent border-light-subtle">
+                <Link to={`/properties/${flat.flatId}`} className="btn btn-outline-primary w-100 mt-2">Visit property</Link>
             </div>
         </div>
     )
