d4ffa14b50a84b374929809a536e7188e6042afe
siegfriedbz <siegfried.bozza@yahoo.com>
2023-06-14 15:23:15 +0200

    refactor context providers in App.tsx

--- app/javascript/common/App.tsx
+++ app/javascript/common/App.tsx
@@ -1,35 +1,56 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { AppContextProvider, UserContextProvider, FlatsContextProvider } from './contexts'
 import Layout from './layout/Layout'
-import { HomePage, AuthPage, CreateFlatPage, FlatDetailsPage, AboutPage, MessagesPage } from './pages'
-import ProtectedRoute from './components/ProtectedRoute'
+import { HomePage, AuthPage, CreateFlatPage, FlatDetailsPage, AboutPage, MessagesPage, NotFoundPage } from './pages'
+import { ProtectedRoute } from './components'
 
 const App: React.FC = () => {
 
     return (
         <Router>
-            <Layout>
-            <div className="container my-2">
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/auth" element={<AuthPage />} />
-                    <Route path="/about" element={<AboutPage />} />
-                    <Route path="/properties/:id" element={<FlatDetailsPage />} />
-                    <Route path="/create-property" element={
-                        <ProtectedRoute>
-                            <CreateFlatPage />
-                        </ProtectedRoute>
-                    } 
-                    />
-                    <Route path="/messages" element={
-                        <ProtectedRoute>
-                            <MessagesPage />
-                        </ProtectedRoute>
-                    } 
-                    />
-                </Routes>
-            </div>
-            </Layout>
+            <AppContextProvider>
+                <UserContextProvider>
+                    <Layout>
+                    <div className="container my-2">
+                        <Routes>
+                            <Route path="about" element={<AboutPage />} />
+                            <Route path="auth" element={<AuthPage />} />
+                            
+                            <Route path="/" element={
+                                <FlatsContextProvider>
+                                    <HomePage />
+                                </FlatsContextProvider>
+                                } 
+                            />
+                            <Route path="properties/:id" element={
+                                <FlatsContextProvider>
+                                    <FlatDetailsPage />
+                                </FlatsContextProvider>
+                                } 
+                            />
+                            <Route path="create-property" element={
+                                <FlatsContextProvider>
+                                    <ProtectedRoute>
+                                        <CreateFlatPage />
+                                    </ProtectedRoute>
+                                </FlatsContextProvider>
+                                } 
+                            />
+
+                            <Route path="messages" element={
+                                <ProtectedRoute>
+                                    <MessagesPage />
+                                </ProtectedRoute>
+                            } 
+                            />
+
+                            <Route path="*" element={<NotFoundPage />} />
+                        </Routes>
+                    </div>
+                    </Layout>
+                </UserContextProvider>
+            </AppContextProvider>
         </Router>
     )
 }
