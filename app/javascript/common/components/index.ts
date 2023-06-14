5827fd5b71f9c2ce4f252613a042b6ee6f332550
siegfriedbz <siegfried.bozza@yahoo.com>
2023-06-14 15:26:38 +0200

    add NotFoundPage and route

--- app/javascript/common/components/index.ts
+++ app/javascript/common/components/index.ts
@@ -1,8 +1,10 @@
+import ProtectedRoute from './ProtectedRoute'
 import FlashMessage from './FlashMessage'
 import FlatForm from './flats/FlatForm'
 import FlatsContainer from './flats/FlatsContainer'
 
 export {
+    ProtectedRoute,
     FlashMessage,
     FlatForm,
     FlatsContainer
