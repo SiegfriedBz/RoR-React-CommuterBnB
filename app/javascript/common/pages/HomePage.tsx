843583db69da90921f738617fdf3ff7824a9382e
siegfriedbz <siegfried.bozza@yahoo.com>
2023-06-20 09:31:33 +0200

    update HomePage

--- app/javascript/common/pages/HomePage.tsx
+++ app/javascript/common/pages/HomePage.tsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useCallback } from 'react'
 import { useFlatsContext } from '../contexts'
-import { FlatsContainer } from '../components'
+import { FlatsContainer } from '../components/flats'
 
 const HomePage = () => {
     // useEffect fetch flats from server & set context
     useFlatsContext()
 
     return (
-        <>
+        <div className="home-page--wrapper">
             <FlatsContainer />
-        </>
+        </div>
     )
 }
 
