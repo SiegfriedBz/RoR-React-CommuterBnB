03836c5689aaa08e3a051eb1c3960c5755f52a5e
siegfriedbz <siegfried.bozza@yahoo.com>
2023-06-20 13:06:24 +0200

    fix user schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_20_093032) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_20_094435) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,7 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_20_093032) do
     t.string "street"
     t.string "city", null: false
     t.string "country", null: false
-    t.index ["user_id"], name: "index_flats_on_user_id", unique: true
+    t.index ["user_id"], name: "index_flats_on_user_id"
   end
 
   create_table "messages", force: :cascade do |t|
