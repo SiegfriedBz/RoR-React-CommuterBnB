32d7313fec0f180ca7c2a70aa08cd17dcd3917cd
siegfriedbz <siegfried.bozza@yahoo.com>
2023-06-20 09:16:17 +0200

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,9 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
 
 # db/seeds.rb
 puts '===================='
 puts 'Start seeding...'
-
-# Create Users
-admin1 = User.create(email: 'admin1@example.com', password: 'password', role: :admin)
-user1 = User.create(email: 'user1@example.com', password: 'password', role: :user)
-user2 = User.create(email: 'user2@example.com', password: 'password', role: :user)
 
 # Create Flats
 def attach_images(flat, filenames)
@@ -30,8 +20,7 @@ def attach_images(flat, filenames)
 end
 
 # flat1 user1
-flat1 = Flat.create(
-  user: user1,
+flat1 = Flat.new(
   title: 'Cosy Flat in Valletta',
   description: 'Awesome flat in Valletta',
   address: 'Valletta, Malta',
@@ -43,8 +32,7 @@ filenames = ['greece_a01.jpg', 'greece_a02.jpg', 'greece_a03.jpg', 'greece_a04.j
 attach_images(flat1, filenames)
 
 # flat2 user1
-flat2 = Flat.create(
-  user: user1,
+flat2 = Flat.new(
   title: 'Cosy Room in Heraklion',
   description: 'Awesome room in Heraklion',
   address: 'Heraklion, Greece',
@@ -56,8 +44,7 @@ filenames = ['greece_b01.jpg', 'greece_b02.jpg', 'greece_b03.jpg', 'greece_b04.j
 attach_images(flat2, filenames)
 
 # flat3 user1
-flat3 = Flat.create(
-  user: user1,
+flat3 = Flat.new(
   title: 'Cosy Flat in Mylos',
   description: 'Awesome flat in Mylos',
   address: 'Mylos, Greece',
@@ -69,8 +56,7 @@ filenames = ['greece_a01.jpg', 'greece_a02.jpg', 'greece_a03.jpg', 'greece_a04.j
 attach_images(flat3, filenames)
 
 # flat4 user1
-flat4 = Flat.create(
-  user: user1,
+flat4 = Flat.new(
   title: 'Cosy Room in Mykonos',
   description: 'Awesome room in Mykonos',
   address: 'Mykonos, Greece',
@@ -82,8 +68,7 @@ filenames = ['greece_b01.jpg', 'greece_b02.jpg', 'greece_b03.jpg', 'greece_b04.j
 attach_images(flat4, filenames)
 
 # flat5 user2
-flat5 = Flat.create(
-  user: user2,
+flat5 = Flat.new(
   title: 'Cosy Flat in Paros',
   description: 'Awesome flat in Paros',
   address: 'Paros, Greece',
@@ -95,8 +80,7 @@ filenames = ['greece_c01.jpg', 'greece_c02.jpg', 'greece_c03.jpg', 'greece_c04.j
 attach_images(flat5, filenames)
 
 # flat6 user2
-flat6 = Flat.create(
-  user: user2,
+flat6 = Flat.new(
   title: 'Cosy Room in Samos',
   description: 'Awesome room in Samos',
   address: 'Samos, Greece',
@@ -108,8 +92,7 @@ filenames = ['greece_d01.jpg', 'greece_d02.jpg', 'greece_d03.jpg', 'greece_d04.j
 attach_images(flat6, filenames)
 
 # flat7 user2
-flat7 = Flat.create(
-  user: user2,
+flat7 = Flat.new(
   title: 'Cosy Flat in Rhodes',
   description: 'Awesome flat in Rhodes',
   address: 'Rhodes, Greece',
@@ -120,10 +103,9 @@ flat7 = Flat.create(
 filenames = ['greece_c01.jpg', 'greece_c02.jpg', 'greece_c03.jpg', 'greece_c04.jpg', 'greece_c05.jpg']
 attach_images(flat7, filenames)
 
-# flat8 user2
-flat8 = Flat.create(
-  user: user2,
-  title: 'Cosy Room in SamZakynthosos',
+# flat8 user3
+flat8 = Flat.new(
+  title: 'Cosy Room in Zakynthos',
   description: 'Awesome room in Zakynthos',
   address: 'Zakynthos, Greece',
   price_per_night_in_cents: 20000,
@@ -133,38 +115,53 @@ flat8 = Flat.create(
 filenames = ['greece_d01.jpg', 'greece_d02.jpg', 'greece_d03.jpg', 'greece_d04.jpg', 'greece_d05.jpg']
 attach_images(flat8, filenames)
 
-# Print User Attributes
-def print_user_attributes(user)
-    puts "User #{user.id}:"
-    puts "  Email: #{user.email}"
-    puts "  Role: #{user.role}"
-    puts '---'
+# Create Users
+puts 'Creating Users with flats...'
+user01 = User.create(email: 'user01@example.com', password: 'password', role: :user, flats: [flat1, flat2, flat3, flat4])
+user02 = User.create(email: 'user02@example.com', password: 'password', role: :user, flats: [flat5, flat6, flat7])
+user03 = User.create(email: 'user03@example.com', password: 'password', role: :user, flats: [flat8])
+user04 = User.create(email: 'user03@example.com', password: 'password', role: :user)
+admin = User.create(email: 'admin@example.com', password: 'password', role: :admin)
+
+# Print Flat Attributes
+def print_flat_attributes(flat)
+  puts "  Flat #{flat.id}:"
+  puts "   Owner id: #{flat.user_id}"
+  puts "   Title: #{flat.title}"
+  puts "   Description: #{flat.description}"
+  puts "   Address: #{flat.address}"
+  puts "   Latitude: #{flat.latitude}"
+  puts "   Longitude: #{flat.longitude}"
+  puts "   Price per Night in cents: #{flat.price_per_night_in_cents}"
+  puts "   Category: #{flat.category}"
+  puts '---'
 end
 
-users = [admin1, user1, user2]
-# users.each(&:print_user_attributes)
+# Print User Attributes
+def print_user_attributes(user)
+  puts "User id #{user.id}:"
+  puts " Email: #{user.email}"
+  puts " Role: #{user.role}"
+
+  if user.flats.present?
+    puts " Flats:"
+    user.flats.each do |flat|
+      print_flat_attributes(flat)
+    end
+  end
+  puts '---'
+  puts '---'
+end
+
+users = [user01, user02, user03]
 users.each do |user|
     print_user_attributes(user)
 end
 
-# Print Flat Attributes
-def print_flat_attributes(flat)
-    puts "Flat #{flat.id}:"
-    puts "  Title: #{flat.title}"
-    puts "  Description: #{flat.description}"
-    puts "  Address: #{flat.address}"
-    puts "  Latitude: #{flat.latitude}"
-    puts "  Longitude: #{flat.longitude}"
-    puts "  Price per Night in cents: #{flat.price_per_night_in_cents}"
-    puts "  Category: #{flat.category}"
-    puts '---'
-end
-
-flats = [flat1, flat2, flat3, flat4, flat5, flat6, flat7, flat8]
-# flats.each(&:print_flat_attributes)
-flats.each do |flat|
-    print_flat_attributes(flat)
-end
+puts "Admin: User id#{admin.id}:"
+puts "  Email: #{admin.email}"
+puts "  Role: #{admin.role}"
+puts '---'
 
 puts 'Seed data created successfully!'
 puts '===================='
