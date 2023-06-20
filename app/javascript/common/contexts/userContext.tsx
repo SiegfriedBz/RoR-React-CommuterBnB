148cb04340cc45e0992c9c1ff85291266a8c5a32
siegfriedbz <siegfried.bozza@yahoo.com>
2023-06-20 09:29:20 +0200

    fix UserContext

--- app/javascript/common/contexts/userContext.tsx
+++ app/javascript/common/contexts/userContext.tsx
@@ -13,31 +13,37 @@ const UserContext = createContext<any>(null)
 
 export const useUserContext = () => useContext<IUserContext>(UserContext)
 
+const decodeToken = (tokenInStorage: string) => {
+    if (typeof tokenInStorage !== 'string' || tokenInStorage === '{}') return
+
+    const deserializedToken = JSON.parse(tokenInStorage)
+    return jwt_decode(deserializedToken)
+}
+
 export const UserContextProvider: React.FC = ({ children }) => {
-    const [user, setUser] = useState<IUser>(initUser)
 
     const [tokenInStorage, setTokenInStorage] = useLocalStorage('bnbToken', null)
 
-    useEffect(() => {
-        if (typeof tokenInStorage !== 'string' || tokenInStorage === '{}') {
-            setUser(initUser)
-            return
-        }
-        const deserializedToken = JSON.parse(tokenInStorage)
-        const decodedToken = jwt_decode(deserializedToken)
-
-        if(!decodedToken) return 
+    const [user, setUser] = useState<IUser>(() => {
+        const decodedToken = decodeToken((tokenInStorage))
+        if (!decodedToken) return initUser
 
         const { user_id, email, role } = decodedToken
+        return { userId: user_id, email, role }
+    })
 
-        setUser({
-            userId: user_id || undefined,
-            email: email || undefined,
-            role: role || undefined
-        })
+    useEffect(() => {
+        const decodedToken = decodeToken(tokenInStorage)
+        if (!decodedToken) {
+            // if no user logged in || on log out
+            return setUser(initUser)
+        }
+
+        const { user_id, email, role } = decodedToken
+        setUser({ userId: user_id, email, role })
     }, [tokenInStorage])
-
-return (
+    
+    return (
         <UserContext.Provider value={{ user, setTokenInStorage }}>
             {children}
         </UserContext.Provider>
