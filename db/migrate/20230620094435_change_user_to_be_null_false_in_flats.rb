cecba4e057b10bdccc6ca8147ef75d3c9a1e86ee
siegfriedbz <siegfried.bozza@yahoo.com>
2023-06-20 13:08:14 +0200

    fix flats schema

--- db/migrate/20230620094435_change_user_to_be_null_false_in_flats.rb
+++ db/migrate/20230620094435_change_user_to_be_null_false_in_flats.rb
@@ -1,7 +1,5 @@
 class ChangeUserToBeNullFalseInFlats < ActiveRecord::Migration[7.0]
   def change
     change_column_null :flats, :user_id, false
-    remove_index :flats, :user_id
-    add_index :flats, :user_id, unique: true
   end
 end
