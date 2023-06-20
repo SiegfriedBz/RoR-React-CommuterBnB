ce6b7f5efdd727160669a9779a3127ef9ba444a0
siegfriedbz <siegfried.bozza@yahoo.com>
2023-06-20 11:34:59 +0200

    add street, city, country, remove address to user

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -2,8 +2,12 @@ class Flat < ApplicationRecord
     belongs_to :user
     has_many_attached :images
 
-    geocoded_by :address
+    geocoded_by :address_to_geocode
     after_validation :geocode
 
     enum category: { entire_place: 0, private_room: 1 }
+
+    def address_to_geocode
+        [street, city, country].compact.join(', ')
+    end
 end
