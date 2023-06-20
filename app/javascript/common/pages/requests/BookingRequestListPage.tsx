7fd9682c1606571a431635cad2c831b191668929
siegfriedbz <siegfried.bozza@yahoo.com>
2023-06-20 09:27:24 +0200

    add RequestFormsPage + BookingRequestListPage

---
+++ app/javascript/common/pages/requests/BookingRequestListPage.tsx
@@ -0,0 +1,106 @@
+import React, { useRef, useState, useEffect } from 'react'
+import { useFetch } from '../../hooks'
+import { useAppContext, useBookingRequestsContext } from '../../contexts'
+import { FlashMessage } from '../../components'
+import { BookingRequestCard } from '../../components/requests'
+import { MessageFormModalWrapper } from '../../components/messages'
+import MapView from '../../components/map/MapView'
+
+const BookingRequestListPage = () => {
+    //* hooks
+    const topRef = useRef(null)
+    const { getUserTransactionRequests } = useFetch()
+
+    //* contexts
+    const { flashMessage } = useAppContext()
+    const { bookingRequests, setBookingRequests } = useBookingRequestsContext()
+
+    //* state
+    // fly to flat map marker when flat is selected 
+    const [mapSelectedFlatId, setMapSelectedFlatId] = useState<number | undefined>(undefined)
+    // handle modal send message form
+    const [modalIsOpen, setModalIsOpen] = useState(false)
+    const [messageRecipientId, setMessageRecipientId] = useState<number | undefined>(undefined)
+    const [messageRecipientFlatId, setMessageRecipientFlatId] = useState<number | undefined>(undefined)
+    const [messageTransactionRequestId, setMessageTransactionRequestId] = useState<number | undefined>(undefined)
+
+    //* effects
+    useEffect(() => {
+      (async () => {
+        await getAllTransactionRequests()
+      })()
+    }, [])
+
+    const getAllTransactionRequests = async() => {
+      const fetchedTransactionRequests = await getUserTransactionRequests()        
+      if (!fetchedTransactionRequests) return
+
+      const [response, data] = fetchedTransactionRequests
+      if(!data) return
+
+      setBookingRequests(data.transaction_requests)
+    }
+
+    //* methods
+    const handleSendMessage = (recipientId: number, recipientFlatId: number, transactionRequestId: number) => {
+      setMessageRecipientId(recipientId)
+      setMessageRecipientFlatId(recipientFlatId)
+      setMessageTransactionRequestId(transactionRequestId)
+      toggleModal()
+    }
+
+    const toggleModal = () => {
+      setModalIsOpen(prev => !prev)
+    }
+
+    const scrollToTop = () => {
+      topRef.current.scrollIntoView({ behavior: 'smooth' })
+    }
+
+    return (
+        <>
+          <MessageFormModalWrapper 
+            modalIsOpen={modalIsOpen}
+            toggleModal={toggleModal}
+            messageRecipientId={messageRecipientId}
+            messageRecipientFlatId={messageRecipientFlatId}
+            messageTransactionRequestId={messageTransactionRequestId}
+          />
+          <h3 ref={topRef}>Booking requests</h3>
+          {flashMessage.message && <FlashMessage {...flashMessage} />}
+          <div className="row">
+            {bookingRequests.length === 0 ?
+              ( <span className="d-block my-1">You don't have active booking requests yet,
+                <span className="text-info fw-bolder"> start exploring the properties on the map
+                </span>
+                </span>
+              )
+              :
+              ( <div className={`col-12 ${bookingRequests.length > 0 && "col-xl-6"}`}>
+                {bookingRequests?.map((bookingRequest) => {
+                  return (
+                    <BookingRequestCard
+                        key={bookingRequest.transactionRequestId}
+                        transactionRequest={bookingRequest}
+                        setMapSelectedFlatId={setMapSelectedFlatId}
+                        handleSendMessage={handleSendMessage}
+                    />
+                  )
+                })}
+              </div>
+              )
+            }
+            <div className={`col-12 ${bookingRequests.length > 0 && "col-xl-6"}`}>
+              <div className='sticky-sm-top'>
+                <MapView mapHeight={700} mapSelectedFlatId={mapSelectedFlatId} />
+                <div className='d-flex justify-content-end mt-1'>
+                  <button className="btn bi-arrow-up btn-primary" onClick={scrollToTop}>Scroll</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+    )
+}
+
+export default BookingRequestListPage
---
+++ app/javascript/common/pages/requests/RequestFormsPage.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { NavLink, useParams, Outlet, Navigate } from 'react-router-dom'
+import { useFlatsContext } from '../../contexts'
+import { FlatDescription } from '../../components/flats'
+import { HostedBy } from '../../components'
+import { LoadingSpinners } from '../../components'
+
+const RequestFormsPage = () => {
+    const { id: selectedFlatId } = useParams()
+    // contexts
+    const { flats } = useFlatsContext()
+
+    if(!selectedFlatId) return <LoadingSpinners />
+
+    const flat = flats.find(flat => flat.flatId === parseInt(selectedFlatId))
+
+    if(!flat) return <Navigate to="/" replace={true}/>
+    
+    return (
+        <div className="mb-3">
+            <FlatDescription flat={flat} />
+        <div className="mb-3">
+            <HostedBy />
+        </div>
+        <div className="d-flex mb-3">
+            <NavLink 
+                to="booking"
+                className="btn btn-outline-primary me-2">
+                Send a booking request
+            </NavLink>
+            <NavLink
+                to="message"
+                className="btn btn-outline-primary">
+                Send a message 
+            </NavLink>
+        </div>
+        <Outlet />
+      </div>
+    ) 
+}
+
+export default RequestFormsPage
