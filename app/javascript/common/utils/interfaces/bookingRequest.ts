bd8a5a9eb6f584337e339388be6898f817e017c6
siegfriedbz <siegfried.bozza@yahoo.com>
2023-06-24 17:28:54 +0200

    add IBookingRequest interface

---
+++ app/javascript/common/utils/interfaces/bookingRequest.ts
@@ -0,0 +1,13 @@
+export interface IBookingRequest {
+    transactionRequestId: number,
+    updatedAt: string,
+    startingDate: string,
+    endingDate: string,
+    exchangePricePerNightInCents: number,
+    responderAgreed: boolean,
+    initiatorAgreed: boolean,
+    responderId: number,
+    initiatorId: number,
+    responderFlatId: number,
+    initiatorFlatId?: number
+}
