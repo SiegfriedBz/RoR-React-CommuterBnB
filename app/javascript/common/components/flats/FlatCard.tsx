83bf9455632a8d2b02bbf2511fe873c4ea53bc3d
siegfriedbz <siegfried.bozza@yahoo.com>
2023-06-24 17:17:27 +0200

    update FlatCard

--- app/javascript/common/components/flats/FlatCard.tsx
+++ app/javascript/common/components/flats/FlatCard.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar, faHouse, faDollarSign } from '@fortawesome/free-solid-svg-icons'
 import FlatCardCarousel from './FlatCardCarousel'
 import { IFlat } from '../../utils/interfaces'
 
@@ -14,19 +16,34 @@ const FlatCard: React.FC<IProps> = ({ flat, flatCardOnMap }) => {
 
     return (
         <div className={`flat-card--wrapper ${flatCardOnMap ? "flat-card-on-map" : ""} h-100`}>
-            <div className="flat-card--info card-body">
-                <h6 className="card-title text-info fw-bolder">{title}</h6>
+            <div className="flat-card--dark card-body">
+                <h6 className="card-title text-dark fw-bolder">{title}</h6>
                 <div className="d-flex justify-content-between align-content-center">
-                    {address && <span className="card-text justify-content-start">{address}</span>}
-                    <span className="card-text">$ {pricePerNightInCents/100}</span>
+                    {   address && 
+                        <span className="card-text justify-content-start">
+                            <FontAwesomeIcon icon={faHouse} />
+                            {" "}{address}
+                        </span>
+                    }
+                    <span className="card-text">
+                        <FontAwesomeIcon icon={faStar} />
+                        <FontAwesomeIcon icon={faStar} />
+                        <FontAwesomeIcon icon={faStar} />
+                        <FontAwesomeIcon icon={faStar} />
+                        <FontAwesomeIcon icon={faStar} />
+                    </span>
+                    <span className="card-text">
+                        <FontAwesomeIcon icon={faDollarSign} />
+                        {pricePerNightInCents/100}
+                    </span>
                 </div>
             </div>
             {flat?.images && <FlatCardCarousel key={flatId} images={flat.images} className="card-img-top" />}
             <div className="flat-card--link card-footer bg-transparent border-light-subtle">
                 <Link 
                     to={`/properties/${flatId}`} 
-                    className="btn btn-sm btn-outline-primary w-100 mt-2"
-                >Visit property
+                    className="btn btn-sm btn-outline-dark w-100 mt-2"
+                >Visit
                 </Link>
             </div>
         </div>
