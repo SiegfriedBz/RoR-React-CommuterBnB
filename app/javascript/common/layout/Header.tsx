c104baadb9afdea58cbfcf08d6abe364d78e2af2
siegfriedbz <siegfried.bozza@yahoo.com>
2023-06-24 17:25:14 +0200

    update Header

--- app/javascript/common/layout/Header.tsx
+++ app/javascript/common/layout/Header.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import { Link, NavLink } from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+    faUser,
+    faEnvelope,
+    faReceipt,
+    faMoneyCheck,
+    faHouseCircleCheck,
+    faRightFromBracket,
+    faRightToBracket
+} from '@fortawesome/free-solid-svg-icons'
 import { useUserContext } from '../contexts'
 
-const Header = () => {
+const Header: React.FC = () => {
     const { user, setTokenInStorage } = useUserContext()
     
     const handleLogout = () => {
@@ -13,7 +23,7 @@ const Header = () => {
         <header>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to='/'>logo</Link>
+                    <Link className="navbar-brand" to='/'>SwapBnb</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                             data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
                             aria-label="Toggle navigation">
@@ -27,11 +37,31 @@ const Header = () => {
                                     menu
                                 </a>
                                 <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                    <li><NavLink className="dropdown-item" to='/my-profile'>Profile</NavLink></li>  
-                                    <li><NavLink className="dropdown-item" to='/my-messages'>Messages</NavLink></li>
-                                    <li><NavLink className="dropdown-item" to='/my-booking-requests'>Booking requests</NavLink></li>
-                                    <li><NavLink className="dropdown-item" to='/my-payments'>Payments</NavLink></li>
-                                    <li><NavLink className="dropdown-item" to='/add-property'>Add a Property</NavLink></li>
+                                    <li>
+                                        <NavLink className="dropdown-item" to='/my-profile'>
+                                        <FontAwesomeIcon icon={faUser} />{" "}Profile
+                                        </NavLink>
+                                    </li>  
+                                    <li>
+                                        <NavLink className="dropdown-item" to='/my-messages'>
+                                            <FontAwesomeIcon icon={faEnvelope} />{" "}Messages
+                                        </NavLink>
+                                    </li>
+                                    <li>
+                                        <NavLink className="dropdown-item" to='/my-booking-requests'>
+                                            <FontAwesomeIcon icon={faReceipt} />{" "}Booking requests
+                                        </NavLink>
+                                    </li>
+                                    <li>
+                                        <NavLink className="dropdown-item" to='/my-payments'>
+                                            <FontAwesomeIcon icon={faMoneyCheck} />{" "}Payments
+                                        </NavLink>
+                                    </li>
+                                    <li>
+                                        <NavLink className="dropdown-item" to='/add-property'>
+                                            <FontAwesomeIcon icon={faHouseCircleCheck} />{" "}Add property
+                                        </NavLink>
+                                    </li>
                                 </ul>
                             </li>
                             <li className="nav-item">
@@ -44,14 +74,14 @@ const Header = () => {
                                         <span 
                                             className="nav-link"
                                             onClick={handleLogout}   
-                                        >Logout
+                                        ><FontAwesomeIcon icon={faRightFromBracket} />
                                         </span>
                                     </div>
                                     :
                                     <NavLink 
                                         className="nav-link"
                                         to='/auth'
-                                    >signup/login
+                                    ><FontAwesomeIcon icon={faRightToBracket} />
                                     </NavLink>
                                 }
                             </li>
