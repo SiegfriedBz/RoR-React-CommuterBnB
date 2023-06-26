6ca04be88cf977db12352347f30565c1f0ab9f07
siegfriedbz <siegfried.bozza@yahoo.com>
2023-06-26 14:16:14 +0200

    fix imports

--- app/javascript/common/components/index.ts
+++ app/javascript/common/components/index.ts
@@ -2,10 +2,12 @@ import ProtectedRoute from './ProtectedRoute'
 import LoadingSpinners from './LoadingSpinners'
 import HostedBy from './HostedBy'
 import TotalPriceAndDays from './TotalPriceAndDays'
+import ScrollToTopButton from "./ScrollToTopButton"
 
 export {
     ProtectedRoute,
     LoadingSpinners,
     HostedBy,
-    TotalPriceAndDays
+    TotalPriceAndDays,
+    ScrollToTopButton
 }
--- app/javascript/common/utils/interfaces/index.ts
+++ app/javascript/common/utils/interfaces/index.ts
@@ -1,5 +1,5 @@
 import { IFlashMessage, IAppContext } from "./appInterfaces"
-import { IUser, IUserContext } from "./userInterfaces"
+import { IUser, IUserContext, IMessage, IConversation } from "./userInterfaces"
 import { IFlat, IFlatsContext, FlatCategoryType } from "./flatsInterfaces"
 import { IBookingRequest } from "./bookingRequest"
 
@@ -8,6 +8,8 @@ export {
     IAppContext,
     IUser,
     IUserContext,
+    IMessage,   
+    IConversation,
     IFlat,
     IFlatsContext,
     FlatCategoryType,
