d96c5b8954746fecd2ba0db927c233c9a27ead5a
siegfriedbz <siegfried.bozza@yahoo.com>
2023-06-27 14:01:10 +0200

    use websocket pushed msg to update conversations context

--- app/javascript/common/contexts/messagesContext.tsx
+++ app/javascript/common/contexts/messagesContext.tsx
@@ -1,42 +1,18 @@
-import React, { useState, useEffect, createContext, useContext } from "react"
+import React, { useRef, useState, useEffect, createContext, useContext } from "react"
 import { v4 as uuid } from 'uuid'
 import { useFetch } from "../hooks"
 import { useAppContext } from "./appContext"
 import { useUserContext } from "./userContext"
+import { formatMessagesAndSetConversations } from './helpers/formatMessagesAndSetConversations'
 import { 
+    IUser,
     IFlashMessage,
+    IIncomingMessage,
     IMessage,
     IConversation,
     IMessagesContext,
     IMessagesChannelsKeys
  } from '../utils/interfaces'
-import { formatMessagesAndSetConversations } from './helpers/formatMessagesAndSetConversations'
-
-interface INewMessage {
-    messageId?: number,
-    content?: string,
-    authorUserId?: number,
-    authorEmail?: string,
-    recipientUserId?: number,
-    recipientEmail?: string,
-    messageFlatId?: number,
-    transactionRequestId?: number,
-    createdAt?: string,
-    updatedAt?: string
-}
-
-const initNewMessage = {
-    messageId: undefined,
-    content: undefined,
-    authorUserId: undefined,
-    authorEmail: undefined,
-    recipientUserId: undefined,
-    recipientEmail: undefined,
-    messageFlatId: undefined,
-    transactionRequestId: undefined,
-    createdAt: undefined,
-    updatedAt: undefined
-}
 
 const MessagesContext = createContext(null)
 
@@ -51,7 +27,7 @@ export const MessagesContextProvider: React.FC = ({ children }) => {
     //# state
     // set conversations from fetched messages
     const [conversations, setConversations] = useState<IConversation[] | undefined>(undefined)
-    const [newMessage, setNewMessage] = useState<INewMessage>(initNewMessage)
+    const notificationRef = useRef<IIncomingMessage | undefined>(undefined)
 
     //# state
     // websocket
@@ -67,7 +43,12 @@ export const MessagesContextProvider: React.FC = ({ children }) => {
             const fetchedData = await getUserMessages()
             if(!fetchedData) return
 
-            const conversations = formatMessagesAndSetConversations(fetchedData)
+            const [response, data] = fetchedData
+            if(!response.ok || !data?.messages) return
+        
+            const fetchedMessages = data?.messages
+
+            const conversations = formatMessagesAndSetConversations(fetchedMessages)
             setConversations(conversations)
         })()
     }, [token])
@@ -109,38 +90,25 @@ export const MessagesContextProvider: React.FC = ({ children }) => {
             if(data.type === "welcome") return
             if(data.type === "confirm_subscription") return
             
-            const { message } = data
-            if(!message) return
+            // if(!!data?.message) return
+            const inComingMessage: IIncomingMessage =  data.message.message
+            const inComingConversation: IConversation = formatMessagesAndSetConversations([inComingMessage])
+            const inComingConversationKey = Object.keys(inComingConversation)[0]
+            const inComingConversationValue = inComingConversation[inComingConversationKey]
 
-            const { 
-                message: {
-                    id: messageId,
-                    author: { userId: authorUserId, email: authorEmail },
-                    recipient: { userId: recipientUserId, email: recipientEmail },
-                    content,
-                    flatId: messageFlatId,
-                    transactionRequestId,
-                    createdAt,
-                    updatedAt
-                }
-                } = message
-
-
-            const newMessage: INewMessage = {
-                messageId,
-                content,
-                authorUserId,
-                authorEmail,
-                recipientUserId,
-                recipientEmail,
-                messageFlatId,
-                transactionRequestId,
-                createdAt,
-                updatedAt
+            // set notification if user is recipient
+            if(user?.userId === inComingMessage?.recipient?.userId) {
+                notificationRef.current = inComingMessage
             }
+
+            const newConversations = Object.keys(conversations).reduce((acc, key) => {
+                return key === inComingConversationKey ?
+                { ...acc, [key]: [ ...conversations[key], ...inComingConversationValue] }
+                : { ...acc, [key]: conversations[key] }
+            }, {})
             
-            // set new message
-            setNewMessage(newMessage)
+            // set conversations
+            setConversations(newConversations)
         }
         
         // return () => {
@@ -152,39 +120,22 @@ export const MessagesContextProvider: React.FC = ({ children }) => {
         setWs(ws)
     }, [user, token, messagesChannelsKeys])
 
-    // on newMessage: fetch all messages & set conversations
     useEffect(() => {
-        if(!newMessage?.messageId) return
-        
-        //  & set notification to recipient
-        (async () => {
-            const fetchedData = await getUserMessages()
-            
-            if(!fetchedData) return
+        if(!user?.userId) return
+        if(!notificationRef.current?.author) return
 
-            const conversations = formatMessagesAndSetConversations(fetchedData)
+        const { author: { email: authorEmail } } = notificationRef.current
 
-            setConversations(conversations)
-        })()
-    }, [newMessage])
+        const authorName = authorEmail.split("@")[0]
+        const notificationMessage = `New message from ${authorName}`
 
-    // on newMessage: set notification to recipient
-    useEffect(() => {
-        if(!newMessage?.messageId) return
-        
-        const { authorEmail, recipientUserId } = newMessage
+        setFlashMessage({ message: notificationMessage, type: "success"})
+    }, [user, notificationRef.current])
 
-        if(user?.userId === recipientUserId) {
-            const authorName = authorEmail.split("@")[0]
-            const notificationMessage = `New message from ${authorName}`
-            setFlashMessage({ message: notificationMessage, type: "success"})
-        }
-
-        setNewMessage(initNewMessage)
-    }, [newMessage])
-
+    
     return (
         <MessagesContext.Provider value={{
+            notificationRef,
             conversations,
             setConversations,
         }}>
