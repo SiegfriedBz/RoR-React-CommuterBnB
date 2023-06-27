7571b505cefee5ba533bc4ee8955d3c592c99bfe
siegfriedbz <siegfried.bozza@yahoo.com>
2023-06-27 13:49:46 +0200

    update scrollToTop Btn

--- app/javascript/common/components/ButtonScrollToTop.tsx
+++ app/javascript/common/components/ButtonScrollToTop.tsx
@@ -1,24 +1,26 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowUpRightDots } from '@fortawesome/free-solid-svg-icons'
+import ButtonSlide from "../components/ButtonSlide"
 
 interface IProps {
     scrollToTop: () => void
 }
 
-const ScrollToTopButton: React.FC<IProps> = (props) => {
+const ButtonScrollToTop: React.FC<IProps> = (props) => {
     const { scrollToTop } = props
 
     return (
         <div className='d-flex justify-content-end my-1'>
-            <button 
-                className="btn text-white fw-bolder btn-dark"
+            <ButtonSlide 
+                className="top-slide"
+                type="button"
                 onClick={scrollToTop}
             >
                 <FontAwesomeIcon icon={faArrowUpRightDots} />
-            </button>
+            </ButtonSlide>
         </div>
     )
 }
 
-export default ScrollToTopButton
+export default ButtonScrollToTop
