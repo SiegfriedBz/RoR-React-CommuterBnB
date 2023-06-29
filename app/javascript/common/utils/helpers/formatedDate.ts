a67b8ddd0b1a99b02d0bc30f8dad59920f2b6f69
siegfriedbz <siegfried.bozza@yahoo.com>
2023-06-29 18:02:02 +0200

    fix formatedDate

--- app/javascript/common/utils/helpers/formatedDate.ts
+++ app/javascript/common/utils/helpers/formatedDate.ts
@@ -1,3 +1,4 @@
+import moment from 'moment';
 export const formatedDate = (dateStr) => {
-    return new Date(dateStr).toLocaleDateString("en-US", { month: "long", year: "numeric" })
+    return moment(dateStr).format("MMM Do YY");  
 }
