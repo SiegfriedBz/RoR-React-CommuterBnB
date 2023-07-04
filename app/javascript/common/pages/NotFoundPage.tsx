2b3fe632f4c8fd59b267d215cb97de4368ed7f13
siegfriedbz <siegfried.bozza@yahoo.com>
2023-07-04 14:41:12 +0200

    update NotFoundPage

--- app/javascript/common/pages/NotFoundPage.tsx
+++ app/javascript/common/pages/NotFoundPage.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Link } from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAnglesLeft } from '@fortawesome/free-solid-svg-icons'
+
 
 const NotFoundPage = () => {
   return (
-    <div>NotFoundPage</div>
+    <div>
+      <h1>404 - Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+      <Link to="/" replace={true} className="text-decoration-none">
+        <FontAwesomeIcon icon={faAnglesLeft} />
+        {" "}Back</Link>
+    </div>
   )
 }
 
