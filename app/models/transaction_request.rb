67988ddc07c1ec89a80c33eeaf2158d6ada3432f
siegfriedbz <siegfried.bozza@yahoo.com>
2023-07-05 21:01:54 +0200

    refactor/fix models + serializers

--- app/models/transaction_request.rb
+++ app/models/transaction_request.rb
@@ -5,40 +5,33 @@ class TransactionRequest < ApplicationRecord
     # belongs_to :initiator_flat, class_name: 'Flat', optional: true
 
     scope :initiator_or_responder_for_user, -> (user_id) { where("responder_id = :user_id OR initiator_id = :user_id", user_id: user_id) }
-
-    scope :completed_for_flat, -> (flat) { where("responder_flat_id = :flat_id OR initiator_flat_id = :flat_id", flat_id: flat.id).where(status: :completed) }
+    
+    # scope :paid_and_future, -> () { where(status: TransactionRequest.statuses[:completed]).where('starting_date > :today', today: Date.today) }
+    scope :paid_and_future, -> () { where(status: :completed).where('starting_date > :today', today: Date.today) }
     
     # validates :status, inclusion: { in: %i[pending completed rejected] }
   
     enum status: { pending: 0, completed: 1, rejected: 2 }
 
-
-    # check in a transaction request is flats are bookable for the given dates
-    def is_bookable_for_dates?
-        
+    # check in a transaction request if paybale === if flats are bookable for the given dates
+    def flats_bookable_for_dates?
         starting_date = self.starting_date
         ending_date = self.ending_date
 
-        
         initiator_flat = Flat.find_by(id: self.initiator_flat_id)
         responder_flat = Flat.find_by(id: self.responder_flat_id)
         
-      
-        responder_flat_is_bookable = responder_flat.bookable_for?(starting_date, ending_date)
-        
+        responder_flat_is_bookable = responder_flat.bookable_for_dates?(starting_date, ending_date)
 
         if initiator_flat.present?
-            
-            initiator_flat_is_bookable = initiator_flat.bookable_for?(starting_date, ending_date)
+            initiator_flat_is_bookable = initiator_flat.bookable_for_dates?(starting_date, ending_date)
             return responder_flat_is_bookable && initiator_flat_is_bookable
         else
-            
-            return responder_flat_is_bookable
+            responder_flat_is_bookable
         end
     end
 
     def users_agreed?
-        
         self.initiator_agreed? && self.responder_agreed?  
     end
 end
--- app/serializers/flat_serializer.rb
+++ app/serializers/flat_serializer.rb
@@ -12,23 +12,34 @@ class FlatSerializer
 
   attribute :future_booked_dates do |flat|
     flat.future_booked_dates_for_flat
+    # TransactionRequest
+    #   .where(status: TransactionRequest.statuses[:completed])
+    #   .where('starting_date >= :today', today: Date.today)
+    #   .where('initiator_flat_id = :flat_id OR responder_flat_id = :flat_id', flat_id: flat.id)
+    #   .map { |transaction_request| { starting_date: transaction_request.starting_date, ending_date: transaction_request.ending_date } }
   end
 
   attribute :reviews do |flat|
-    flat.flat_reviews
-      .order(updated_at: :desc)
-      .map do |review| 
-      ReviewSerializer.new(review).serializable_hash[:data][:attributes]
+    flat_reviews = flat.reviews
+
+    if flat_reviews.present?
+      flat_reviews = flat_reviews
+        .order(updated_at: :desc)
+        .map do |review| 
+          ReviewSerializer.new(review).serializable_hash[:data][:attributes]
+        end
     end
+
+    flat_reviews || []
   end
 
   attribute :average_rating do |flat|
-    rating_sum = flat.flat_reviews.map do |review| 
+    rating_sum = flat.reviews.map do |review| 
       review.rating
     end.sum
     
     if rating_sum > 0
-      rating_sum / flat.flat_reviews.count
+      rating_sum / flat.reviews.count
     else
       5
     end
