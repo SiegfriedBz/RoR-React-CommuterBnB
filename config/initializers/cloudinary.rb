8ac2b80a62f65541ca16ec29caa66f4a44f6101f
siegfriedbz <siegfried.bozza@yahoo.com>
2023-07-06 20:32:10 +0200

    update Cloudinary cfg

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,7 +1,8 @@
 Cloudinary.config do |config|
-    config.cloud_name = ENV['cloud_name']
-    config.api_key = ENV['cloud_api_key']
-    config.api_secret = ENV['cloud_api_secret']
+    config.cloud_name = Rails.application.credentials.fetch(:cloud_name)
+    config.api_key = Rails.application.credentials.fetch(:cloud_api_key)
+    config.api_secret = Rails.application.credentials.fetch(:cloud_api_secret)
     config.secure = true
     config.cdn_subdomain = true
    end
+   
